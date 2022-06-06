f251a4a7a26b25cb8c495feaa7d14f3d37515532
Jmorg21 <Jmorg21@gmail.com>
2022-06-05 15:17:37 -0900

    simplifying folders

---
+++ Assets/script.js
@@ -0,0 +1,46 @@
+
+$('#currentDay').text(moment().format('dddd, MMMM Do'));
+//function to update color//
+function updateColor() {
+  var currentHour = moment().hour();
+  
+  $('.time-block').each(function(){
+      var hourRow = parseInt($(this).attr("id"));
+
+      if(hourRow < currentHour) {
+        $(this).addClass('past');
+      }
+      else if(hourRow === currentHour) {
+          $(this).removeClass('past');
+          $(this).addClass('present');
+      }
+      else{
+          $(this).removeClass('past');
+          $(this).removeClass('present');
+          $(this).addClass('future');
+      }
+  })
+};
+//save button and local storage//
+$(document).ready(function () {
+  $(".saveBtn").on('click', function () {
+    var text = $(this).siblings('description').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(time, text);
+  })
+
+  $("#8 .description").val(localStorage.getItem("8"));
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
+//calling color update function//
+  updateColor()
+})
+
